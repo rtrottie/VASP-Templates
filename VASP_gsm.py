88940eeade2b602d5b659efebc104baecca12c02
ryan <themeisme@gmail.com>
2016-06-21 09:20:21 -0600

    for GSM script, runs vasp occasionally checking for optimal NUPDOWN

--- VASP_gsm.py
+++ VASP_gsm.py
@@ -3,9 +3,34 @@ import os
 import shutil
 from custodian.vasp.jobs import *
 from custodian.vasp.handlers import *
+import numpy as np
 from custodian.custodian import *
 from Classes_Custodian import *
 from Classes_Pymatgen import *
+
+def energy():
+    '''
+    Gets energy for run in current directory
+
+    :return:
+    '''
+    v = Vasprun('vasprun.xml', parse_dos=False, parse_eigen=False)
+    return v.final_energy
+
+def run_vasp(override=[], suffix=''):
+    '''
+    execute vasp with given override and suffix
+
+    :param override:
+    :param suffix:
+    :return:
+    '''
+
+    handlers = []
+    vaspjob = [StandardJob([vasp, '-np', os.environ['VASP_PROCS'], vasp], 'vasp.log', auto_npar=False, backup=False,
+                           settings_override=override, suffix=suffix)]
+    c = Custodian(handlers, vaspjob, max_errors=10)
+    c.run()
 
 shutil.copy('../../INCAR', 'INCAR')
 shutil.copy('../../KPOINTS', 'KPOINTS')
@@ -15,16 +40,42 @@ try:
 except:
         pass
 
+# Determine wheter to use Gamma optimized vasp
 incar = Incar.from_file('INCAR')
-handlers = []
 if 'AUTO_GAMMA' in incar and incar['AUTO_GAMMA']:
     vasp = os.environ['VASP_GAMMA']
 else:
     vasp = os.environ['VASP_KPTS']
-if 'AUTO_NUPDOWN' in incar:
 
 
+# Check for NUPDOWN
+if os.path.exists('nupdown_info'):  # Determine if full check must be done or if just using past NUPDOWN
+    with open('nupdown_info') as f:
+        lines = f.readlines()
+        nupdown_best = lines[0]  # optimal NUPDOWN is first line
+        nupdown_iters = lines[1] # number of iterations with best nupdown
+    if nupdown_iters < incar['AUTO_NUPDOWN_ITERS']:
+        nupdown_check = True
+    else:
+        nupdown_check = False
+else:
+    nupdown_check = True
 
-vaspjob = [StandardJob([os.environ['VASP_MPI'], '-np', os.environ['VASP_PROCS'], vasp], 'vasp.log', auto_npar=False, backup=False)]
-c = Custodian(handlers, vaspjob, max_errors=10)
-c.run()
+if 'AUTO_NUPDOWN' in incar and nupdown_check: # have a guess of nupdown
+    pass
+elif 'AUTO_NUPDOWN' in incar and not nupdown_check: # First run in new folder
+        energies = []
+        for nupdown in incar['AUTO_NUPDOWN']:  # get energies for each desired NUPDOWN
+            override = [{"dict": "INCAR",
+                         "action": {"_set": {"NUPDOWN": nupdown,
+                                             'ISTART': 0,        # Don't use previously converged WAVECAR and CHGCAR
+                                             'ICHARG': 2}}}]
+            run_vasp(override, '.' + str(nupdown))
+            energies.append(energy())
+        with open('nupdown_info', 'w') as f: # store info for later use
+            i = np.argmin(energies) # get index minima
+            f.writelines(str([incar['AUTO_NUPDOWN'][i]]), '0')
+else:
+    run_vasp()
+
+
