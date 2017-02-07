52be62b6d57c1bdb93a947ba357294dbe18000e6
Ryan Trottier <themeisme@gmail.com>
2017-02-07 13:22:12 -0700

    upper

--- VASP_pylada.py
+++ VASP_pylada.py
@@ -68,7 +68,7 @@ def fix_incar(incar: Incar):
     #     if bad in incar:
     #         incar[good] = incar[bad]
     #         del incar[bad]
-    if 'GGA' in incar and 'P' == incar['GGA'][0] and 'S' in incar['GGA'][1]:
+    if 'GGA' in incar and 'P' == incar['GGA'][0].upper() and 'S' in incar['GGA'][1].upper():
         incar['GGA']='PS'
 
     return incar
