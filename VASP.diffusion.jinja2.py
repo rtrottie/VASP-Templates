677ca2083ca5dbf901ef9722493f6f44a7ac8e34
Ryan Trottier <themeisme@gmail.com>
2017-09-21 11:30:18 -0600

    Diffusion fxn

---
+++ VASP.diffusion.jinja2.py
@@ -0,0 +1,47 @@
+{% extends "VASP.base.jinja2.sh" %}
+{% block python %}
+from custodian.custodian import *
+from Classes_Custodian import *
+import Upgrade_Run
+import logging
+import copy
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO, filename='run.log')
+jobtype = '{{ jobtype }}'
+vasp_kpts = '{{ vasp_kpts }}'
+vasp_gamma =  '{{ vasp_gamma }}'
+nsteps = 10
+
+
+handlers = [WalltimeHandler({{ time }}*60*60, min(30*60, {{ time }}*60*60/20), electronic_step_stop=True)]
+continuation = []
+continuation.append({'file': os.path.join('01', 'CONTCAR'),
+                     'action': {'_file_copy': {'dest': os.path.join('POSCAR')}}})
+
+
+def get_runs(max_steps=1000):
+    for i in range(max_steps):
+        if i > 0 and ((not os.path.exists('CONTCAR') or os.path.getsize('CONTCAR') == 0) and (not os.path.exists('01/CONTCAR') or os.path.getsize('01/CONTCAR') == 0)):
+            raise Exception('empty CONTCAR')
+        incar = Incar.from_file('INCAR')
+        try:
+            nebef = subprocess.Popen('nebef.pl')
+            force = float(nebef[0].split()[1])
+            logging.info('Force:  {}'.format(force))
+            if force < -incar['EDIFFG']:
+                final = True
+            else:
+                final = False
+        except:
+            final = False
+        if ('AUTO_GAMMA' in incar and incar['AUTO_GAMMA']):
+            vasp = vasp_gamma
+        else:
+            vasp = vasp_kpts
+        yield DiffusionJob(vasp_cmd=['{{ mpi }}', '-np', '{{ tasks }}', vasp], output_file='{{ logname }}', auto_npar=False, final=final)
+
+
+c = Custodian(handlers, get_runs(), max_errors=10)
+c.run()
+{% endblock python %}
