686bad377190ef77758b41808bfaf893537bc80a
Ryan Trottier <themeisme@gmail.com>
2017-11-02 08:38:00 -0600

    use energy as convergence

--- VASP.plane_constrained.jinja2.py
+++ VASP.plane_constrained.jinja2.py
@@ -9,9 +9,10 @@ export VASP_PROCS={{ tasks }}
 {% block python %}
 from Classes_ASE import StandardVasp as Vasp
 from Classes_ASE import InPlane as KeepInPlane
+from Classes_ASE import converged_fmax_as_emax
 from Classes_Pymatgen import Incar
 from ase.io import read
-from tsase.optimize import SDLBFGS as Optimizer
+from ase.optimize.precon import PreconFIRE as Optimizer
 
 atoms = read('POSCAR')
 atoms.set_calculator(Vasp())
@@ -25,6 +26,7 @@ c = KeepInPlane(i['DIFFATOM'], (i['CONSATOM1'], i['CONSATOM2'], i['CONSATOM3']))
 atoms.set_constraint(c)
 
 dyn = Optimizer(atoms, trajectory='run.traj', restart='history.pckl')
-dyn.run(emax=1e-6)
+dyn.converged = converged_fmax_as_emax
+dyn.run(fmax=i['EDIFFG'])
 
 {% endblock python %}
