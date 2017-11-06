e01d2c695ce84f5dd65ff0101cfd089c2299f077
Ryan Trottier <themeisme@gmail.com>
2017-11-06 15:12:04 -0700

    two atom plane_constrained

--- VASP.plane_constrained.jinja2.py
+++ VASP.plane_constrained.jinja2.py
@@ -8,11 +8,20 @@ export VASP_PROCS={{ tasks }}
 
 {% block python %}
 from Classes_ASE import StandardVasp as Vasp
-from Classes_ASE import InPlane as KeepInPlane
 from Classes_ASE import converged_fmax_as_emax
 from Classes_Pymatgen import Incar
 from ase.io import read
 from ase.optimize.precon import PreconFIRE as Optimizer
+
+i = Incar.from_file('INCAR')
+if 'CONSATOM3' in i:
+    from Classes_ASE import InPlane as KeepInPlane
+    c = KeepInPlane(i['DIFFATOM'], (i['CONSATOM1'], i['CONSATOM2'], i['CONSATOM3']))
+else:
+    from Classes_ASE import InMPPlane as KeepInPlane
+    c = KeepInPlane(i['DIFFATOM'], (i['CONSATOM1'], i['CONSATOM2']))
+
+
 
 atoms = read('POSCAR')
 atoms.set_calculator(Vasp())
@@ -22,7 +31,6 @@ i['IBRION'] = -1
 if 'IOPT' in i:
     del i['IOPT']
 i.write_file('INCAR')
-c = KeepInPlane(i['DIFFATOM'], (i['CONSATOM1'], i['CONSATOM2'], i['CONSATOM3']))
 atoms.set_constraint(c)
 
 Optimizer.converged = converged_fmax_as_emax
