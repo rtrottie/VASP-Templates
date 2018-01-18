8dad6ad376c7d9658ecec11e4c5db11d55d8eb81
Ryan Trottier <themeisme@gmail.com>
2018-01-18 11:32:32 -0700

    iterate

--- VASP.plane_constrained.jinja2.py
+++ VASP.plane_constrained.jinja2.py
@@ -13,6 +13,7 @@ from Classes_Pymatgen import Incar
 from ase.io import read
 from ase import Atoms
 from ase.optimize.precon.fire import PreconFIRE as Optimizer
+import numpy as np
 import logging
 import sys
 
@@ -26,22 +27,64 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
+iterate = None
 i = Incar.from_file('INCAR')
-atoms = read('POSCAR')
+atoms = read('POSCAR')# type: Atoms
 if 'CONSATOM3' in i and 'CONTINUE_3PT' in i:
     from Classes_ASE import LockedTo3AtomPlane
     print('3 Atom Constraint cont.')
     a = read('POSCAR')# type: Atoms
     c = LockedTo3AtomPlane(i['DIFFATOM'], (i['CONSATOM1'], i['CONSATOM2'], i['CONSATOM3']), a.positions[i['DIFFATOM']])
 if 'CONSTYPE' in i and i['CONSTYPE'] == 'Bond':
-    if 'BOND1' in i and 'BOND2' in i:
+    if 'ITERATE' in i and i['ITERATE']:
         from ase.constraints import FixInternals
         print('Constraining Bonds to fixed length')
+        bond1 = atoms.get_distance(i['DIFFATOM'], i['CONSATOM1'], mic=True)
+        bond2 = atoms.get_distance(i['DIFFATOM'], i['CONSATOM2'], mic=True)
         bonds = [
-            [ i['BOND1'], [ i['DIFFATOM'], i['CONSATOM1'] ] ],
-            [ i['BOND2'], [ i['DIFFATOM'], i['CONSATOM2'] ] ]
+            [ bond1, [ i['DIFFATOM'], i['CONSATOM1'] ] ],
+            [ bond2, [ i['DIFFATOM'], i['CONSATOM2'] ] ]
                 ]
         c = FixInternals(bonds=bonds)
+        def iterate(atoms : Atoms):
+            delta = 0.01
+            global bond1
+            global bond2
+            force = atoms.get_forces()[i['DIFFATOM']]
+            bond1_dist   = atoms.get_distance(i['DIFFATOM'], i['CONSATOM1'], vector=False)
+            bond1_vector = atoms.get_distance(i['DIFFATOM'], i['CONSATOM1'], vector=True )
+            bond1_unit = bond1_vector / bond1_dist
+            bond2_dist   = atoms.get_distance(i['DIFFATOM'], i['CONSATOM2'], vector=False)
+            bond2_vector = atoms.get_distance(i['DIFFATOM'], i['CONSATOM2'], vector=True )
+            bond2_unit = bond2_vector / bond2_dist
+
+            bond1_projection = np.dot(force, bond1_unit)
+            bond2_projection = np.dot(force, bond2_unit)
+            if abs(bond1_projection) > bond2_projection:
+                if bond1_projection > 0:
+                    print('Growing Bond 1')
+                    bond1 += delta
+                    bond2 -= delta / 4
+                else:
+                    print('Shrinking Bond 1')
+                    bond1 -= delta
+                    bond2 += delta / 4
+            else:
+                if bond2_projection > 0:
+                    print('Growing Bond 2')
+                    bond2 += delta
+                    bond1 -= delta / 4
+                else:
+                    print('Shrinking Bond 2')
+                    bond2 -= delta
+                    bond1 += delta / 4
+            bonds = [
+                [ bond1, [ i['DIFFATOM'], i['CONSATOM1'] ] ],
+                [ bond2, [ i['DIFFATOM'], i['CONSATOM2'] ] ]
+                    ]
+            return FixInternals(bonds=bonds)
+
+
     else:
         from ase.constraints import FixBondLengths
         print('Constraining Bonds')
@@ -73,6 +116,11 @@ atoms.set_constraint(c)
 Optimizer.converged = converged_fmax_or_emax
 dyn = Optimizer(atoms, trajectory='run.traj', restart='history.pckl')
 dyn.run(fmax=-i['EDIFFG'])
+if iterate:
+    print('Converged')
+    dyn = Optimizer(atoms, trajectory='run.traj', restart='history.pckl')
+    dyn.run(fmax=-i['EDIFFG'])
+    atoms = dyn.atoms()
+    c = iterate()
 print('Done')
-
 {% endblock python %}
