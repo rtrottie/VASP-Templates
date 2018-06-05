40433f9baeeecb081331af977e735b38750a90c9
Ryan Trottier <themeisme@gmail.com>
2018-06-05 15:23:24 -0600

    better buffer time formating

--- VASP_pylada.py
+++ VASP_pylada.py
@@ -64,12 +64,13 @@ def run_vasp(override=[], suffix=''):
         start_time = int(os.environ['PBS_START_TIME'])
         current_time = calendar.timegm(time.gmtime())
         elapsed_time = current_time - start_time
-        walltime = int(os.environ['PBS_WALLTIME']) - elapsed_time
+        walltime = int(os.environ['PBS_WALLTIME'])
+        remaining_time = walltime - elapsed_time
         logging.info('Walltime : {}'.format(walltime))
         buffer_time = min(45 * 60, walltime * 60 * 60 / 20)
-        if buffer_time*5 > walltime:
+        if buffer_time > walltime:
             raise Exception('Not Enough Time')
-        handlers += [WalltimeHandler(wall_time=walltime, buffer_time=buffer_time, electronic_step_stop=True,)]
+        handlers += [WalltimeHandler(wall_time=remaining_time, buffer_time=buffer_time, electronic_step_stop=True,)]
     if ('IMAGES' in incar and 'ICHAIN' in incar) and (incar['IMAGES'] == 1 and incar['ICHAIN'] == 0):
         if 'POSCAR' in os.listdir():
             inital_images =  ceil((incar['IMAGES'] - 1) / 2) + 1
