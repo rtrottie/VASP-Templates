139e72329180d61d06b9ca6ad69bec70c58021b9
Ryan Trottier <themeisme@gmail.com>
2018-08-08 13:46:32 -0600

    full converge to ediffg -0.02

--- VASP.multistep.jinja2.py
+++ VASP.multistep.jinja2.py
@@ -58,7 +58,7 @@ def get_runs(max_steps=100):
         else:
             final = False
         if ('AUTO_GAMMA' in incar and incar['AUTO_GAMMA']):
-            vasp = vasp_gamma
+            vasp = os.environ['VASP_GAMMA']
         else:
             vasp = vasp_kpts
         yield job(['{{ mpi }}', '-np', '{{ tasks }}', vasp], '{{ logname }}', auto_npar=False, settings_override=settings, final=final)
