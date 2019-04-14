2075b43a326537aac1835b989413e9b4ab9946d8
Ryan Trottier <themeisme@gmail.com>
2019-04-14 13:37:51 -0600

    remove handlers

--- VASP_gsm.py
+++ VASP_gsm.py
@@ -41,6 +41,7 @@ def run_vasp(override=[], suffix=''):
         vasp = os.environ['VASP_KPTS']
 
     handlers = [VaspErrorHandler(output_filename='vasp.log')]
+    handlers = []
     if os.environ['VASP_MPI'] == 'srun':
         vaspjob = [StandardJob(['srun', vasp], 'vasp.log', auto_npar=False, backup=False,
                                settings_override=override, suffix=suffix, final=False)]
